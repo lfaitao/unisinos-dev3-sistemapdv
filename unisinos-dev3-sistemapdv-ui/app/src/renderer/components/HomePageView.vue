498b0dadaddcbb827691ebabd788cea8a9b9d844
Leo Faitao <lfaitao@gmail.com>
2017-04-04 23:24:54 -0300

    Tela inicial com os botões de acesso para as paginas

---
+++ unisinos-dev3-sistemapdv-ui/app/src/renderer/components/HomePageView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div style="height: 100%; background-color: #EEE;">
+      <md-layout md-gutter md-align="center" style="height: 100%">
+        <md-layout md-column md-align="center" style="height: 100%; align-items: center">
+          <md-card-header style="text-align: center; padding-bottom: 20px">
+            <md-card-header-text>
+              <h1>Sistema PDV</h1>
+            </md-card-header-text>
+          </md-card-header>
+          <md-card md-align="center" style="background-color: rgba(158, 158, 158, 0.35)">
+              <md-button class="md-raised md-primary">Gerenciar Caixa</md-button>
+              <md-button class="md-raised md-primary">Gerenciar Vendas</md-button>
+              <md-button class="md-raised md-primary">Gerenciar Pré-Vendas</md-button>
+              <md-button class="md-raised md-primary">Gerenciar DAVs</md-button>
+              <md-button class="md-raised md-primary">Gerenciar Usuários</md-button>
+          </md-card>
+        </md-layout>
+      </md-layout>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'home-page'
+  }
+</script>
+
+<style scoped>
+  img {
+    margin-top: -25px;
+    width: 450px;
+  }
+</style>
